2975e585282cf3845cda5a7ae0515051899a646d
artemlagun <artemlagun12@gmail.com>
2022-11-16 09:23:56 +0300

    2_01_data_jpa

--- src/main/java/ru/javaops/bootjava/BootJavaApplication.java
+++ src/main/java/ru/javaops/bootjava/BootJavaApplication.java
@@ -1,14 +1,30 @@
 package ru.javaops.bootjava;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import ru.javaops.bootjava.model.Role;
+import ru.javaops.bootjava.model.User;
+import ru.javaops.bootjava.repository.UserRepository;
+
+import java.util.Set;
 
 @SpringBootApplication
-public class BootJavaApplication extends SpringBootServletInitializer {
+@AllArgsConstructor
+public class BootJavaApplication implements ApplicationRunner {
+
+    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(BootJavaApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", Set.of(Role.ROLE_USER)));
+        userRepository.save(new User("admin@javaops.ru", "Admin_First", "Admin_Last", "admin", Set.of(Role.ROLE_USER, Role.ROLE_ADMIN)));
+        System.out.println(userRepository.findAll());
+    }
 }
